d626daf18ad6f623e3816fc7533fea93f5a918f1
shadya7med <57808802+shadya7med@users.noreply.github.com>
2021-02-18 21:56:40 +0200

    empty project first commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Shady Ahmed on 2/18/21.
+//  Copyright © 2021 Shady Ahmed. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
